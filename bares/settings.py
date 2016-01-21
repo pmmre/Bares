8e969b35eb49589df159faf621f106ee0ac10180
pmmre <pmmr@correo.ugr.es>
2016-01-21 16:55:07 +0100

    probando

--- bares/settings.py
+++ bares/settings.py
@@ -7,34 +7,11 @@ https://docs.djangoproject.com/en/1.7/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
-# Parse database configuration from $DATABASE_URL
 import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
- 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')    
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'h27xhcx6&6dtem=@&z($t*73)1(&-&ico--17_21+(yvv7c_t@'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-
-from unipath import Path
-RUTA_PROYECTO = Path(__file__).ancestor(2)
-
-ALLOWED_HOSTS = ['*']
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-#import os
 import django
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -46,11 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'r%4)vjbf#=g_t#v6612lj5xit4%_3w*jces_6_o+j6#k)!2ot&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,10 +104,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
-
-
+STATIC_URL = '/static/'
 
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
